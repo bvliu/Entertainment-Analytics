114ca338a496967468d475b007b3038bfdfc7239
Brandon Liu <liu.b97@gmail.com>
2016-06-24 13:48:04 -0400

    database work

--- IndividualURL.py
+++ IndividualURL.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib.parse
 import urllib.request
+import re
+from datetime import datetime
 
-url = "http://www.boxofficemojo.com/movies/?id=horsemoney.htm"
+url = "http://www.boxofficemojo.com/movies/?id=slumdogmillionaire.htm"
 #url = "http://www.boxofficemojo.com/yearly/chart/?page=1&view=releasedate&view2=domestic&yr=2015&p=.htm"
 
 with urllib.request.urlopen(url) as response:
@@ -13,18 +15,67 @@ with urllib.request.urlopen(url) as response:
 
 # Use Beautiful Soup to organize the html into a more readable way
 soup = BeautifulSoup(html, 'html.parser')
+#print (soup.encode("utf-8"))
+
+# BETTER WAY TO DO IT:
+info = soup.find_all('b')
+# x = 0
+# for item in info:
+# 	print (item.encode('utf-8'))
+# 	print (str(x) + " " + item.getText())
+# 	x+=1
+
+def readStaticData():
+	# 1 = title
+
+	title = info[1].getText()
+	print ("Title: " + title)
+
+	# 2 = domestic total as of some date
+
+	total = info[2].getText()
+	total = re.sub('[$,]','',total)
+	total = int(total)
+	print ("Domestic Total Gross: " + str(total))
+
+	# 3 = distributor
+
+	distributor = info[3].getText()
+	print ("Distributor: " + distributor)
+
+	# 4 = release date
+
+	release = info[4].getText()
+	print ("Release Date: " + release)
+	release = re.sub("[,]","",release)
+	print (release)
+	release = datetime.strptime(release, "%B %d %Y")
+	print(release.strftime("%Y-%m-%d"))
 
 
-# TITLE -----------------------------------------------------------------
-title = soup.title.getText()
+	# 5 = Genre
 
-#print(title.find(" - Box Office Mojo")) --> 18
-#print(title[:18]) but still has the parentheses
+	genre = info[5].getText()
+	print ("Genre: " + genre)
 
-# Slice from the beginning of the parentheses
-title = title[:title.find(" (")]
-print (title)
+	# 6 = Runtime
 
-# BUDGET ----------------------------------------------------------------
+	runtime = info[6].getText()
+	print(runtime)
+	runtime = runtime.split(' ')
+	print(runtime)
 
-print (soup.find_all("b")[3].getText())
+	print ("Runtime: " + runtime)
+
+	# 7 = Rating
+
+	rating = info[7].getText()
+	print ("Rating: " + rating) 
+
+	# 8 = Production Budget
+
+	budget = info[8].getText()
+	print ("Production Budget: " + budget)
+
+readStaticData()
+
--- scraper.py
+++ scraper.py
@@ -2,40 +2,51 @@ from bs4 import BeautifulSoup
 import urllib.parse
 import urllib.request
 import pymysql.cursors
+import re
 
-# List of the 2015 top grossing films, which will be parsed for the URLs
-url = "http://www.boxofficemojo.com/yearly/chart/?page=1&view=releasedate&view2=domestic&yr=2015&p=.htm"
+BASE_YEAR = "1980"
+CURRENT_YEAR = "2016"
+
 
 # General http address for Box Office Mojo
-bom = "http://www.boxofficemojo.com"
+BOM = "http://www.boxofficemojo.com"
 
-# Now, when the “with” statement is executed, Python evaluates the expression, 
-# calls the __enter__ method on the resulting value (which is called a 
-# “context guard”), and assigns whatever __enter__ returns to the variable 
-# given by as. Python will then execute the code body, and no matter what 
-# happens in that code, call the guard object’s __exit__ method.
-with urllib.request.urlopen(url) as response:
+# Iterate through every year
+def baseScrapingLoop():
 
-	# gets the raw html code from the URL
-	html = response.read()
-	#print (html)
+	# Make sure we start off with a blank SQL database CHANGE THIS LATER
+	emptyTable()
 
-# Use Beautiful Soup to organize the html into a more readable way
-soup = BeautifulSoup(html)
-organized = soup.prettify() 	
-#print(organized)
+	# Loop through each year starting from the first year (1980) to the current year, set above
+	for year in range(int(BASE_YEAR), int(CURRENT_YEAR) + 1):
 
-# Return a list of the movie URLs from html soup
-def getMovieURLs():
+		# List of the (year) top grossing films, which will be parsed for the URLs
+		url = "http://www.boxofficemojo.com/yearly/chart/?page=1&view=releasedate&view2=domestic&yr=1980&p=.htm"
+
+		# Now, when the “with” statement is executed, Python evaluates the expression, 
+		# calls the __enter__ method on the resulting value (which is called a 
+		# “context guard”), and assigns whatever __enter__ returns to the variable 
+		# given by as. Python will then execute the code body, and no matter what 
+		# happens in that code, call the guard object’s __exit__ method.
+		with urllib.request.urlopen(url) as response:
+
+			# gets the raw html code from the URL
+			html = response.read()
+			#print (html)
+
+		# Use Beautiful Soup to organize the html into a more readable way
+		soup = BeautifulSoup(html)
+		organized = soup.prettify() 	
+		#print(organized)	
+
+		scrapeMoviesFromYear(url)
+
+
+# Iterate through each of the individual pages under the year, 0-100 101-200 etc.
+def scrapeMoviesFromYear(url):
 
 	# Keep track of the current page
 	currentPage = 0
-#
-	# URL that will be edited 
-	url = "http://www.boxofficemojo.com/yearly/chart/?page=1&view=releasedate&view2=domestic&yr=2015&p=.htm"
-
-	# Create the list
-	urlList = []
 
 	# Want to find every movie url for the given year
 	while True:
@@ -61,15 +72,76 @@ def getMovieURLs():
 			# not ref = ft is a special case that we don't want to be in the list
 			if 'movies' in name and 'ref=ft' not in name: 
 				url = bom + name
-				urlList.append(url) # Add it to the list, maybe should do some sorting here?
 				scrapeDataFromMovie(url)
 
 
-	#print (urlList)
-	putURLsIntoDatabase(urlList)
-
 def scrapeDataFromMovie(url):
 
+	with urllib.request.urlopen(url) as response:
+
+		# gets the raw html code from the URL
+		html = response.read()
+		#print (html)
+
+		# Use Beautiful Soup to organize the html into a more readable way
+		soup = BeautifulSoup(html, 'html.parser')
+		#print (soup.encode("utf-8"))
+
+		readStaticData(soup.find_all('b'))
+
+
+def readStaticData(info):
+	# 1 = title
+
+	title = info[1].getText()
+	print ("Title: " + title)
+
+	# 2 = domestic total as of some date
+	# Comes in this format: $141,319,928, needs to be made an int
+
+	total = int(re.sub('[$,]', '', info[2].getText()))
+	print ("Domestic Total Gross: " + str(total))
+
+	# 3 = distributor
+
+	distributor = info[3].getText()
+	print ("Distributor: " + distributor)
+
+	# 4 = release date
+	# Format Release Date: November 12, 2008 needs to be in yyyy-mm-dd
+
+	release = re.sub("[,]", "", info[4].getText())
+	release = datetime.strptime(release, "%B %d %Y")
+	release = release.strftime("%Y-%m-%d")
+	print ("Release Date: " + release)
+
+	# 5 = Genre
+
+	genre = info[5].getText()
+	print ("Genre: " + genre)
+
+	# 6 = Runtime
+
+	runtime = info[6].getText()
+	print ("Runtime: " + runtime)
+
+	# 7 = Rating
+
+	rating = info[7].getText()
+	print ("Rating: " + rating) 
+
+	# 8 = Production Budget
+
+	budget = info[8].getText()
+	print ("Production Budget: " + budget)
+
+	connection = pymysql.connect(host = 'localhost', user = 'root',
+		password = 'pass', db = 'test', charset = 'utf8mb4',
+		cursorclass = pymysql.cursors.DictCursor) 
+
+	try:
+		with connection.cursor() as cursor:
+			sql = "INSERT INTO" `movies` ()
 
 
 def putURLsIntoDatabase(myList):
@@ -104,7 +176,22 @@ def putURLsIntoDatabase(myList):
 		connection.close()
 
 
+def emptyTable():
+	connection = pymysql.connect(host = 'localhost', user = 'root',
+	password = 'pass', db = 'test', charset = 'utf8mb4',
+	cursorclass = pymysql.cursors.DictCursor) 
+
+	try:
+		with connection.cursor() as cursor:
+			# Clears the table
+			sql = "TRUNCATE `movies`"
+			cursor.execute(sql)
+		connection.commit()
+	finally:
+		connection.close()
 
 
 
-getMovieURLs()
+
+
+baseUrlLoop()
