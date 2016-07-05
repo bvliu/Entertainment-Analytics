4a2728ab123b30e5fee495a628e6c88f22d701be
Courtneyp123 <courtney.peng@gmail.com>
2016-07-05 13:26:53 -0400

    So far: Basic IMDb API information and Production

--- imdb.py
+++ imdb.py
@@ -1,11 +1,13 @@
 import urllib.request
 import json
+from bs4 import BeautifulSoup 
+
+#pymysql.connect 
 
 x=0
 
 out = open('C:\\Users\\582406\\Documents\\Courtney 2016\\DataScrapeExample\\outfileEx.txt','w')
 error=open('C:\\Users\\582406\\Documents\\Courtney 2016\\DataScrapeExample\\errorEx.txt','w')
-
 
 f=open('C:\\Users\\582406\\Documents\\Courtney 2016\\DataScrapeExample\\dummyList.txt')
 line=f.readline()
@@ -33,9 +35,40 @@ while line:
 		out.write(json_obj['Awards']+'\t')
 		out.write(json_obj['imdbVotes']+'\t')
 		out.write(json_obj['imdbRating']+'\t')
-		out.write(json_obj['imdbID']+'\n') 
+		out.write(json_obj['imdbID']+ "\t") 
+
+		id=json_obj['imdbID']
+		#sets the id for later - prodIMDb.py 
+
+		url2="http://www.imdb.com/title/"
+		url2+=id 
+		url2+="/companycredits?ref_=tt_dt_co"
+
+		
+		readURL=urllib.request.urlopen(url2).read()	
+
+		soup=BeautifulSoup(readURL, "html.parser")
+
+		tableStats=soup.find("ul", { "class" : "simpleList"})
+
+
+		for row in tableStats.find_all('a'):
+
+			#***just testing *** 
+	
+			try:
+				name=row.getText()
+				name=name.replace("\n","")
+				out.write(name + ",")
+				print(name)
+
+			except Exception as e: 
+				pass
+
+		out.write("\n")
 
 	except Exception as e: 
+		print("Exception")
 		pass
 
 	line=f.readline()
